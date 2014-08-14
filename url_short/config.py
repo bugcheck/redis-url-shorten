07630a4b608127da77c39dc2d4f29a86ee0ae8e9
Raj Rao <neeraj2608@gmail.com>
2014-08-14 17:48:33 -0400

    Removing obsolete redistogo url.

---
+++ url_short/config.py
@@ -0,0 +1,7 @@
+DEBUG = True
+SECRET_KEY = '' # enter secret key
+REDIS_URL = '' # enter redis url
+
+MSG_UNKNOWN_SHORT_URL = 'Unknown short URL'
+MSG_ID_REQ_PARAM_MISSING = 'id request param missing.'
+MSG_ALREADY_SHORT_URL = 'Already a short URL.'
